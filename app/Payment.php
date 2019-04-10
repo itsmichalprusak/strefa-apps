08c04be6a15e89d971bf9190eca0408e107f5097
Michał Prusak <meehoweq@gmail.com>
2019-04-10 20:38:41 +0200

    Nadpisanie kodu boot w modelach dla generowania UUID

--- app/Payment.php
+++ app/Payment.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Webpatser\Uuid\Uuid;
 
 class Payment extends Model
 {
-    //
+    /**
+     *  Setup model event hooks
+     */
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->uuid = (string) Uuid::generate(4);
+        });
+    }
 }
